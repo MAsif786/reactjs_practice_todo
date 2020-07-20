d33dd8a26d9b4d8d80a7410de01fa43e77323d34
Muhammad Asif <snew3110@gmail.com>
2020-07-21 04:46:54 +0500

    todo component add and passes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Todos from './components/Todos'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+  // test = 
+  const [todos, setTodos] = useState(
+    {
+      todos:[
+        {
+          id : 1,
+          title : 'Please Sleep at time'
+        },
+        {
+          id : 2,
+          title : 'Go to sleep'
+        },
+        {
+          id : 3,
+          title : 'I am done.'
+        }
+      ]
+    }
+  )
+
   return (
+
     <div className="App">
-      <h1>Hello React</h1>
+      <Todos todos={todos.todos} />
     </div>
   );
 }
